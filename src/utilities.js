4cb40670b0167fd49cb4d443b4ae92cf2b265ae0
dilshanhiruna <dilshan.hirunaa@gmail.com>
2021-07-01 22:31:31 +0530

    implemented draw path

--- src/utilities.js
+++ src/utilities.js
@@ -38,14 +38,43 @@ export const drawHand = (predictions, ctx) => {
   if (predictions.length > 0) {
     predictions.forEach((prediction) => {
       const landmarks = prediction.landmarks;
+      // Loop through fingers
+      for (let j = 0; j < Object.keys(fingerJoints).length; j++) {
+        let finger = Object.keys(fingerJoints)[j];
+        //  Loop through pairs of joints
+        for (let k = 0; k < fingerJoints[finger].length - 1; k++) {
+          // Get pairs of joints
+          const firstJointIndex = fingerJoints[finger][k];
+          const secondJointIndex = fingerJoints[finger][k + 1];
 
+          // Draw path
+          ctx.beginPath();
+          ctx.moveTo(
+            landmarks[firstJointIndex][0],
+            landmarks[firstJointIndex][1]
+          );
+          ctx.lineTo(
+            landmarks[secondJointIndex][0],
+            landmarks[secondJointIndex][1]
+          );
+          ctx.strokeStyle = "plum";
+          ctx.lineWidth = 4;
+          ctx.stroke();
+        }
+      }
+
+      // Loop through landmarks and draw em
       for (let i = 0; i < landmarks.length; i++) {
+        // Get x point
         const x = landmarks[i][0];
+        // Get y point
         const y = landmarks[i][1];
+        // Start drawing
         ctx.beginPath();
-        ctx.arc(x, y, 5, 0, 3 * Math.PI);
+        ctx.arc(x, y, style[i]["size"], 0, 1 * Math.PI);
 
-        ctx.fillStyle = " aqua";
+        // Set line color
+        ctx.fillStyle = style[i]["color"];
         ctx.fill();
       }
     });
